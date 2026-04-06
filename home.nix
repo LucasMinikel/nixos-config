828cafea0b96eae26fde0bb5c7dbd13834a127f4
Lucas Minikel <lucasminikel.m@gmail.com>
2026-04-06 17:23:00 -0300

    Preserve default Hyprland config and append keybind snippet source

--- home.nix
+++ home.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ lib, ... }:
 
 {
   home.username = "lucas";
@@ -6,6 +6,20 @@
   home.stateVersion = "25.11";
 
   home.file.".config/hypr/conf.d/keybinds.conf".source = ./home/hypr/conf.d/keybinds.conf;
+
+  home.activation.ensureHyprKeybindSource = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+    target="$HOME/.config/hypr/hyprland.conf"
+    source_line='source = ~/.config/hypr/conf.d/keybinds.conf'
+
+    if [ -f "$target" ]; then
+      if ! grep -Fxq "$source_line" "$target"; then
+        echo "$source_line" >> "$target"
+      fi
+    else
+      mkdir -p "$HOME/.config/hypr"
+      printf '%s\n' "$source_line" > "$target"
+    fi
+  '';
 
   programs.home-manager.enable = true;
 }
